0a9694a36ede09ae13ae57a7aff14dffe08c4529
haog29 <hgagaring@yahoo.com>
2019-05-14 18:02:37 -0700

    just for submission

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express()
 var PORT = process.env.PORT || 3000
 
 
-app.use(express.static("app/public"));
+app.use(express.static('../app/public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
